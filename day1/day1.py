bae299932e3d28c64e72d0e07e650b4b038c6f0b
Senek18 <kcisak26@gmail.com>
2022-12-08 15:44:13 +0100

    1 day **

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,22 @@
-def hello_world():
-    print("Hello world!!!")
+input = []
+count = 0
+with open("input.txt", 'r') as file:
+    while True:
+        count += 1
+        line = file.readline()
+        input.append(line.rstrip())
 
-hello_world()
+        if not line:
+            break
+cal = []
+temp = 0
+for i in input:
+    if i == "":
+        cal.append(temp)
+        temp = 0
+    else:
+        temp = temp + float(i)
+print(max(cal))
+
+highest_three = sorted(cal, reverse=True)[:3]
+print(sum(highest_three))
