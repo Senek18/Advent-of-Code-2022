56e5b7b8cc377e1c7b5415bfc2023e52d3806447
Senek18 <kcisak26@gmail.com>
2022-12-11 23:39:15 +0100

    9 day

---
+++ day9/day9.py
@@ -0,0 +1,127 @@
+import numpy as np
+
+moves = []
+
+with open("input_f.txt", 'r') as file:
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        moves.append(line.rstrip())
+
+print(moves)
+
+
+class Move:
+    def __init__(self):
+        self.x_H = 500
+        self.y_H = 500
+        self.x_T = 500
+        self.y_T = 500
+        self.touch = True
+        self.line = True
+        self.map = np.zeros((self.x_H * 2 ,self.y_H * 2))
+
+    def right(self):
+        self.x_H += 1
+        self.check()
+        self.check_line()
+
+        if self.touch is False:
+            if self.line is True:
+                self.x_T += 1
+            else:
+                self.y_T = self.y_H
+                self.x_T += 1
+        self.map[self.x_T, self.y_T] = 1
+
+
+    def left(self):
+        self.x_H -= 1
+        self.check()
+        self.check_line()
+
+        if self.touch is False:
+            if self.line is True:
+                self.x_T -= 1
+            else:
+                self.y_T = self.y_H
+                self.x_T -= 1
+        self.map[self.x_T, self.y_T] = 1
+
+    def up(self):
+        self.y_H += 1
+        self.check()
+        self.check_line()
+
+        if self.touch is False:
+            if self.line is True:
+                self.y_T += 1
+            else:
+                self.x_T = self.x_H
+                self.y_T += 1
+        self.map[self.x_T, self.y_T] = 1
+
+    def down(self):
+        self.y_H -= 1
+        self.check()
+        self.check_line()
+
+        if self.touch is False:
+            if self.line is True:
+                self.y_T -= 1
+            else:
+                self.x_T = self.x_H
+                self.y_T -= 1
+        self.map[self.x_T, self.y_T] = 1
+
+    def check(self):
+        if self.x_H == self.x_T and self.y_H == self.y_T:
+            self.touch = True
+        elif self.x_H + 1 == self.x_T and self.y_H - 1 == self.y_T:
+            self.touch = True
+        elif self.x_H == self.x_T and self.y_H - 1 == self.y_T:
+            self.touch = True
+        elif self.x_H - 1 == self.x_T and self.y_H - 1 == self.y_T:
+            self.touch = True
+        elif self.x_H - 1 == self.x_T and self.y_H == self.y_T:
+            self.touch = True
+        elif self.x_H - 1 == self.x_T and self.y_H + 1 == self.y_T:
+            self.touch = True
+        elif self.x_H == self.x_T and self.y_H + 1 == self.y_T:
+            self.touch = True
+        elif self.x_H + 1 == self.x_T and self.y_H + 1 == self.y_T:
+            self.touch = True
+        else:
+            self.touch = False
+
+    def check_line(self):
+        if self.x_H == self.x_T or self.y_H == self.y_T:
+            self.line = True
+        else:
+            self.line = False
+
+    def show(self):
+        print("Head: ", [self.x_H, self.y_H])
+        print("Tail:", [self.x_T, self.y_T])
+        print(self.map)
+        print(np.sum(self.map))
+
+
+start = Move()
+
+for i in moves:
+    temp = i.split()
+    if temp[0] == "R":
+        for k in range(int(temp[1])):
+            start.right()
+    elif temp[0] == "L":
+        for k in range(int(temp[1])):
+            start.left()
+    elif temp[0] == "U":
+        for k in range(int(temp[1])):
+            start.up()
+    elif temp[0] == "D":
+        for k in range(int(temp[1])):
+            start.down()
+start.show()
---
+++ day9/test.py
@@ -0,0 +1,5 @@
+a = 5
+b = 4
+
+if a == 5 and b == 4:
+    print("OK")
